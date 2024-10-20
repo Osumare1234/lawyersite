9dfb20fda4ca01d2d66dd8005725e8899ba61485
Osumare1234 <emmanuelosumare@outlook.com>
2024-10-20 03:43:12 +0100

    fer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Metadata } from "next";
+import { Navbar } from "../components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-     
+        <Navbar />
         {children}
       </body>
     </html>
